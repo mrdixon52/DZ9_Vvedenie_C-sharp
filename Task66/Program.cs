bf786a5eeede63e53636bc1c5184cae25d6ad0fd
Arseny Zhalnin <mrzhalnin@gmail.com>
2022-12-23 13:21:34 +0300

    Add Task66

---
+++ Task66/Program.cs
@@ -0,0 +1,13 @@
+﻿int rec(int m, int n)
+{
+    if (n == m)
+        return m;
+    return rec(m, n - 1) + n;
+}
+
+Console.Clear();
+Console.Write("Enter number M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(m, n));
